2953cdf5c4a328ba8ef563bca66e3ddec3d20935
Mike <spormuv@gmail.com>
2023-04-16 14:54:38 +0300

    feat: finish task 20

---
+++ 20-useEffect/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [todo, setTodo] = useState(null);
+
+  useEffect(() => {
+    console.log('Callback in useEffect called');
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then((response) => response.json())
+      .then((json) => setTodo(json));
+  }, []);
+
+  console.log('App Rendered');
+  console.log(todo);
+
+  return <div className="App">{todo && <h1>{todo.title}</h1>}</div>;
+}
+
+export default App;
