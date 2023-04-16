c5ccb415dafb4c771c5e947159459c87a7d5829c
Mike <spormuv@gmail.com>
2023-04-16 13:18:21 +0300

    feat: finish task 19

---
+++ 19-children-prop/src/components/Wrapper.js
@@ -0,0 +1,12 @@
+const Wrapper = ({ color, children }) => {
+  const style = {
+    backgroundColor: color,
+    width: '250px',
+    padding: '20px',
+    margin: '20px auto',
+  };
+
+  return <div style={style}>{children}</div>;
+};
+
+export default Wrapper;
