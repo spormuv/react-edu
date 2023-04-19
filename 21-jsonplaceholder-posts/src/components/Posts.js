5f20931bfb17948cf4272d667baccf27d9931c5a
Mike <spormuv@gmail.com>
2023-04-19 12:17:36 +0300

    feat: finish task 21

---
+++ 21-jsonplaceholder-posts/src/components/Posts.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import Post from './Post';
+
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(json => {
+        setPosts(json);
+      })
+      .catch(err => setError(err.message))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  if (error) return <h1>Error: {error}</h1>;
+
+  const postsArray = posts.map(post => <Post key={post.id} {...post} />);
+  return (
+    <>
+      <h1>Posts</h1>
+      <hr />
+      {isLoading ? <h1>Loading...</h1> : postsArray}
+    </>
+  );
+}
+
+export default Posts;
